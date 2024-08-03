45e6b8620bb3820ede1ab033b511c84f8b6678c3
ambineura <46647603+ambineura@users.noreply.github.com>
2024-08-03 04:33:39 +0300

    Update KG_NavUsers.user.js
    fixed bug

--- KG_NavUsers.user.js
+++ KG_NavUsers.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KG_NavUsers
 // @namespace    KG_NavUsers
-// @version      1.0
+// @version      1.1
 // @description  navigate to prev\next user with arrow keys (try to use shift\ctl\alt and combinations)
 // @author       un4given (111001)
 // @match        http*://*.klavogonki.ru/u/*
@@ -12,37 +12,39 @@
 (function() {
     'use strict';
 
-    var curUserID, d, keys=['shiftKey', 'altKey', 'ctrlKey'], nextUserID, r;
+    let curUserID, nextUserID, d, keys=['shiftKey', 'altKey', 'ctrlKey'];
 
     function gotoNextUser(curUserID, nextUserID) {
-        var r = new RegExp(`${curUserID.toString()}`);
+        let r = new RegExp(`${curUserID.toString()}`);
         location.href = location.href.replace(r, nextUserID.toString());
     }
 
     window.addEventListener("keyup", (e) => {
-        switch (e.code) {
-            case 'ArrowLeft':
-            case 'ArrowRight':
-                curUserID = parseInt(location.href.match(/.+?(\d+)/)[1]);
-                d = (e.code == 'ArrowLeft')?-1:1;
+        if (document.activeElement.tagName == 'BODY') {
+            switch (e.code) {
+                case 'ArrowLeft':
+                case 'ArrowRight':
+                    curUserID = parseInt(location.href.match(/.+?(\d+)/)[1]);
+                    d = (e.code == 'ArrowLeft')?-1:1;
 
-                keys.forEach((k) => {if (e[k]) d*=10});
+                    keys.forEach((k) => {if (e[k]) d*=10});
 
-                nextUserID = curUserID + d;
-                gotoNextUser(curUserID, nextUserID);
-                break;
+                    nextUserID = curUserID + d;
+                    gotoNextUser(curUserID, nextUserID);
+                    break;
 
-            case 'ArrowUp':
-            case 'ArrowDown':
-                curUserID = parseInt(location.href.match(/.+?(\d+)/)[1]);
-                d = (e.code == 'ArrowDown')?-10:10;
+                case 'ArrowUp':
+                case 'ArrowDown':
+                    curUserID = parseInt(location.href.match(/.+?(\d+)/)[1]);
+                    d = (e.code == 'ArrowDown')?-10:10;
 
-                keys.forEach((k) => {if (e[k]) d*=10});
+                    keys.forEach((k) => {if (e[k]) d*=10});
 
-                nextUserID = (Math.floor(curUserID/Math.abs(d)))*Math.abs(d);
-                if (Math.sign(d) == 1 || nextUserID == curUserID) nextUserID += d;
-                gotoNextUser(curUserID, nextUserID);
-                break;
+                    nextUserID = (Math.floor(curUserID/Math.abs(d)))*Math.abs(d);
+                    if (Math.sign(d) == 1 || nextUserID == curUserID) nextUserID += d;
+                    gotoNextUser(curUserID, nextUserID);
+                    break;
+            }
         }
     });
 })();
